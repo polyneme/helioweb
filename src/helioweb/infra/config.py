04c95055c08a05b87d6a768d4f2f9a72784849f0
Donny Winston <donny@polyneme.xyz>
2023-12-04 01:17:13 -0800

    feat: tls env var

--- src/helioweb/infra/config.py
+++ src/helioweb/infra/config.py
@@ -1,5 +1,6 @@
 import os
 
 MONGO_HOST = os.environ.get("MONGO_HOST")
+MONGO_TLS = bool(os.environ.get("MONGO_TLS"))
 MONGO_USER = os.environ.get("MONGO_USER")
 MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD")
--- src/helioweb/infra/core.py
+++ src/helioweb/infra/core.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 
-from helioweb.infra.config import MONGO_HOST, MONGO_USER, MONGO_PASSWORD
+from helioweb.infra.config import MONGO_HOST, MONGO_TLS, MONGO_USER, MONGO_PASSWORD
 
 
 def get_mongodb():
     client = MongoClient(
-        host=MONGO_HOST, username=MONGO_USER, password=MONGO_PASSWORD, tls=True
+        host=MONGO_HOST, username=MONGO_USER, password=MONGO_PASSWORD, tls=MONGO_TLS
     )
     mdb = client.helioweb
     return mdb
